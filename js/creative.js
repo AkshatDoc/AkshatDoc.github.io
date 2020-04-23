766e3749fd7f7fefe9bbe8cdb4e7cee75e07bfe3
AkshatDoc <34289525+AkshatDoc@users.noreply.github.com>
2020-04-23 19:48:20 -0400

    UPDATE 2
    Fixed up some things... added photos(finally!)

--- js/creative.js
+++ js/creative.js
@@ -93,3 +93,7 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+function externalLinks() { for (var c = document.getElementsByTagName("a"), a = 0; a < c.length; a++) { var b = c[a];
+        b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank") } };
+externalLinks();
